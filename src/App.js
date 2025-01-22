26160b4de78976e04bdc864197f06e2a0a3beb46
Mario-Gozie <oguedoihucg@gmail.com>
2025-01-22 18:11:49 +0200

    Worked on the About us Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import Home from "./assets/Home/Home";
+import About from "./assets/About/About";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Home />
+      <About />
     </>
   );
 }
---
+++ src/assets/About/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <>
+      <h2>About Me</h2>
+      <p>
+        Hello! I'm a web development student passionate about creating beautiful
+        and functional websites. I am currently learning HTML, CSS and will soon
+        dive into JavaScript
+      </p>
+    </>
+  );
+}
+
+export default About;
